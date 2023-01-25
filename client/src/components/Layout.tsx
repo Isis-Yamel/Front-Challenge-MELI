91ffa5c6bdc045f4727bc2165dd7adefd03b4998
Isis-Yamel <yamelamanca@gmail.com>
2023-01-25 17:34:08 -0500

    * create initial layout, navBar search (header) and fetch api working on search

---
+++ client/src/components/Layout.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+import styles from '@/styles/Layout.module.scss';
+
+import NavBar from '@/components/NavBar';
+
+import { Roboto } from '@next/font/google'
+
+const roboto = Roboto({
+  weight: '100',
+  subsets: ['latin'],
+})
+
+type Props = {
+  children: JSX.Element,
+};
+
+const Layout = ({children}: Props) => {
+  return (
+    <div>
+      <Head>
+        <title>MELI Challenge</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <NavBar />
+      <main className={`${styles.main} ${roboto.className}`}>
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,53 @@
+import Image from 'next/image'
+import React, { useState } from 'react';
+
+import navStyles from '@/styles/NavBar.module.scss';
+
+const NavBar = () => {
+  const [search, setSearch] = useState('');
+  const [data, setData] = useState('');
+
+  const handleSearch = async (e: any) => {
+    e.preventDefault();
+    const res = await fetch(`http://localhost:8000/api/items?q=:${search}`);
+    const data = await res.json();
+
+    setData(data);
+  };
+
+  return (
+    <header className={navStyles.navbar__container}>
+      <Image
+        className={navStyles.navbar__logo}
+        src="/logo.svg"
+        alt="MeLi Logo"
+        width={180}
+        height={80}
+      />
+      <form className={navStyles.navbar__bar__container}>
+        <input 
+          className={navStyles.navbar__bar}
+          type='text'
+          value={search}
+          name='search'
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder='Search here your favorite products'
+        />
+        <button 
+          disabled={search === ''} 
+          onClick={handleSearch} 
+          className={navStyles.navbar__button}
+        >
+          <Image
+            src="/search.png"
+            alt="Search"
+            width={30}
+            height={30}
+          />
+        </button>
+      </form>
+    </header>
+  );
+};
+
+export default NavBar;
